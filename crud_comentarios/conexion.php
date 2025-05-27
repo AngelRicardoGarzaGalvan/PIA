8d32c138d5f11af08b6ab64a3f521e69abc5c34a
AngelRicardoGarzaGalvan <angel.garzagl@uanl.edu.mx>
2025-05-26 19:31:46 -0600

    conexion con base de datos

---
+++ crud_comentarios/conexion.php
@@ -0,0 +1,13 @@
+<?php
+// conexion.php: establece la conexión a la base de datos
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'db_consultoriodental');
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if ($conn->connect_error) {
+    die('Error de conexión: ' . $conn->connect_error);
+}
+?>
+  
