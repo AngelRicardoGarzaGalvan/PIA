f2e88b12d191023c355cd1ccc1ca4b72428a96f3
Israel <israelsifuentes347@gmail.com>
2025-05-26 18:03:16 -0600

    se agrego un redirector para el Secretario, para asegurar que solo ese rol pueda ingresar

---
+++ Secretario/secretario_redirector.php
@@ -0,0 +1,14 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+session_start();
+
+if (!isset($_SESSION['usuario']) || $_SESSION['usuario']['id_rol'] != 4) {
+    echo "<script>alert('Debes iniciar sesión como Secretario para acceder.'); window.location.href = '../index.html';</script>";
+    exit;
+}
+
+// Redirige a la página HTML real
+header("Location: Secretario.html");
+exit;
+?>
